622bd03a4d9c783cf0286a8c30c694adb7765590
Steve Lhomme <robux4@ycbcr.xyz>
2025-06-18 11:21:54 +0200

    buildsystem: remove stray code from patched sqlite
    Following 0f771afdf2020f2413a4cf041f7e2d108f7f36a3 we don't need these parts
    that were added in 11cefbd40cab68c00fdadbe05c1dfd404dea5629.

--- buildsystem/compile-medialibrary.sh
+++ buildsystem/compile-medialibrary.sh
@@ -59,7 +59,6 @@ if [ ! -d "${MEDIALIBRARY_MODULE_DIR}/${SQLITE_RELEASE}" ]; then
   fi
   tar -xozf ${SQLITE_RELEASE}.tar.gz
   rm -f ${SQLITE_RELEASE}.tar.gz
-  cd ${SQLITE_RELEASE}
 fi
 cd ${MEDIALIBRARY_MODULE_DIR}/${SQLITE_RELEASE}
 if [ ! -d "build-$ANDROID_ABI" ]; then
@@ -71,7 +70,6 @@ if [ ! -e ./config.status -o "$RELEASE" = "1" ]; then
   ../configure \
     --host=$TARGET_TUPLE \
     --prefix=${SRC_DIR}/medialibrary/prefix/${TARGET_TUPLE} \
-    --disable-shell \
     --disable-shared \
     CFLAGS="${VLC_CFLAGS}" \
     CXXFLAGS="${VLC_CFLAGS} ${VLC_CXXFLAGS}" \
